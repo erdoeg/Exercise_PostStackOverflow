4fdf1447c11c8208efde73fbeb41f2bab44d6d83
Viviane Erdoeg <Viviane Erdoeg>
2017-08-01 18:53:36 -0300

    Add project files.

---
+++ 05_Exercicio_Posts/Post.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Exercicio_Posts
+{
+    class Posts
+    {
+        public string _title {get; set;}
+        public string _description {get; set;}
+        public DateTime _datePost {get; private set;}
+        public int _vote {get; private set;}
+
+    public Posts()
+        {
+            _title = "Untitled";
+            _datePost = DateTime.Now;
+            _vote = 0;
+            _description = "none";
+        }
+
+        public void AddVote()
+        {
+            _vote++;
+        }
+
+        public void SubVote()
+        {
+            _vote--;
+        }
+
+        public void PostFull()
+        {
+            Console.WriteLine("Posting at StackOverflow");
+            Console.WriteLine("------------------------");
+            Console.WriteLine(_title + " -- {0} Votes", _vote);
+            Console.WriteLine("Posted on - {0}", _datePost.ToShortDateString());
+            Console.WriteLine("------------------------");
+            Console.WriteLine(_description);
+            Console.WriteLine("-----------END----------");
+
+        }
+    }
+}
---
+++ 05_Exercicio_Posts/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Exercicio_Posts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var postNow = new Posts()
+            {
+
+                //postNow._datePost = new DateTime(2017, 1, 1);// nao complia pq é privado
+
+                _title = "Nisi ut aliquid ex ea commodi consequatur?",
+                _description = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat 
+non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            };
+            postNow.AddVote();
+            postNow.AddVote();
+            postNow.AddVote();
+            postNow.AddVote();
+            postNow.AddVote();
+
+            postNow.SubVote();
+
+            postNow.PostFull();
+        }
+    }
+}
